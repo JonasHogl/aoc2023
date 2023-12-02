2ddf07bf9d1b5321b9c47e699e0f318a21bbbe99
JonasHogl <jonas_hoglund_@live.com>
2023-12-02 10:54:46 +0100

    dag1 småfix

--- lib/day1/day1.ex
+++ lib/day1/day1.ex
@@ -5,18 +5,18 @@ defmodule Day1 do
   def del1 do
     Enum.to_list(File.stream!(@fil))
       |> Enum.map(&String.trim/1)
-      |> Enum.map(&filterCharacters/1)
+      |> Enum.map(&filter_characters/1)
       |> Enum.filter(fn x -> String.length(x) > 0 end)
-      |> Enum.map(&mapToCalibrationValue/1)
+      |> Enum.map(&map_to_calibration_value/1)
       |> Enum.map(&String.to_integer/1)
       |> Enum.reduce(fn x, acc -> x + acc end)  
   end
 
-  def filterCharacters(binary) do
+  def filter_characters(binary) do
     String.replace(binary, ~r/[^0-9]/, "")
   end
 
-  def mapToCalibrationValue(data) do
+  def map_to_calibration_value(data) do
     String.first(data) <> String.last(data)
   end
 
@@ -27,9 +27,9 @@ defmodule Day1 do
       |> Enum.map(&String.trim/1)
       |> Enum.map(&String.downcase/1)
       |> Enum.map(&map_spelled_out_numbers/1)
-      |> Enum.map(&filterCharacters/1)
+      |> Enum.map(&filter_characters/1)
       |> Enum.filter(fn x -> String.length(x) > 0 end)
-      |> Enum.map(&mapToCalibrationValue/1)
+      |> Enum.map(&map_to_calibration_value/1)
       |> Enum.map(&String.to_integer/1)
       |> Enum.reduce(fn x, acc -> x + acc end)
   end
@@ -40,22 +40,22 @@ defmodule Day1 do
   defp map_spelled_out_numbers("", res), do: res
   defp map_spelled_out_numbers(string, result) do
     cond do
-      String.starts_with?(string, "one") -> map_spelled_out_numbers(remove_firts_letter(string), result <> "1")
-      String.starts_with?(string, "two") -> map_spelled_out_numbers(remove_firts_letter(string), result <> "2")
-      String.starts_with?(string, "three") -> map_spelled_out_numbers(remove_firts_letter(string), result <> "3")
-      String.starts_with?(string, "four") -> map_spelled_out_numbers(remove_firts_letter(string), result <> "4")
-      String.starts_with?(string, "five") -> map_spelled_out_numbers(remove_firts_letter(string), result <> "5")
-      String.starts_with?(string, "six") -> map_spelled_out_numbers(remove_firts_letter(string), result <> "6")
-      String.starts_with?(string, "seven") -> map_spelled_out_numbers(remove_firts_letter(string), result <> "7")
-      String.starts_with?(string, "eight") -> map_spelled_out_numbers(remove_firts_letter(string), result <> "8")
-      String.starts_with?(string, "nine") -> map_spelled_out_numbers(remove_firts_letter(string), result <> "9")
-      true -> map_spelled_out_numbers(remove_firts_letter(string), result <> String.first(string))
+      String.starts_with?(string, "one") -> map_spelled_out_numbers(string_tail(string), result <> "1")
+      String.starts_with?(string, "two") -> map_spelled_out_numbers(string_tail(string), result <> "2")
+      String.starts_with?(string, "three") -> map_spelled_out_numbers(string_tail(string), result <> "3")
+      String.starts_with?(string, "four") -> map_spelled_out_numbers(string_tail(string), result <> "4")
+      String.starts_with?(string, "five") -> map_spelled_out_numbers(string_tail(string), result <> "5")
+      String.starts_with?(string, "six") -> map_spelled_out_numbers(string_tail(string), result <> "6")
+      String.starts_with?(string, "seven") -> map_spelled_out_numbers(string_tail(string), result <> "7")
+      String.starts_with?(string, "eight") -> map_spelled_out_numbers(string_tail(string), result <> "8")
+      String.starts_with?(string, "nine") -> map_spelled_out_numbers(string_tail(string), result <> "9")
+      true -> map_spelled_out_numbers(string_tail(string), result <> String.first(string))
     end
   end
   
    
 
-  def remove_firts_letter(string) do
+  def string_tail(string) do
     String.slice(string, 1..String.length(string))
   end
 
